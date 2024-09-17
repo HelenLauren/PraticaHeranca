124a2ce09278b1d2d17f4be317e052f2d428c9d5
Helen Lauren Bonato <167373993+HelenLauren@users.noreply.github.com>
2024-09-17 14:04:51 -0300

    Update VeiculoEconomico.java
    IntelliJ nao passou o codigo para o Github.

--- src/VeiculoEconomico.java
+++ src/VeiculoEconomico.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+public class VeiculoEconomico extends Veiculo{
+    private boolean isEletrico;
 
-public class VeiculoEconomico {
+    public VeiculoEconomico(String modelo, String placa, String cor) {
+        super(modelo, placa, cor);
+    }
+
+    public void exibirDetalhes() {
+        super.exibirDetalhes();
+        System.out.println("É elétrico: " + isEletrico);
+
+    }
 }
