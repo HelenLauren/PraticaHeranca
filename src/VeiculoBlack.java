5d9d55d26921685ab7175c9076060935b35a22cc
Helen Lauren Bonato <167373993+HelenLauren@users.noreply.github.com>
2024-09-17 14:04:21 -0300

    Update VeiculoBlack.java
    IntelliJ nao passou o codigo pro Github.

--- src/VeiculoBlack.java
+++ src/VeiculoBlack.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+public class VeiculoBlack extends Veiculo{
+    private boolean temWifi;
+    private boolean temAr;
 
-public class VeiculoBlack {
+    public VeiculoBlack(String modelo, String placa, String cor, boolean temWifi, boolean temAr) {
+        super(modelo, placa, cor);
+        this.temWifi = temWifi;
+        this.temAr = temAr;
+    }
+    public void exibirDetalhes() {
+        super.exibirDetalhes();
+        System.out.println("Possui Wi-Fi: " + temWifi);
+        System.out.println("Possui Ar-condicionado: " + temAr);
+    }
 }
