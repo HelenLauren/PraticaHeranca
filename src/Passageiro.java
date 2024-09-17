2644185fadec47d56a95121fb6892ae8b8f251df
Helen Lauren Bonato <167373993+HelenLauren@users.noreply.github.com>
2024-09-17 14:03:15 -0300

    Update Passageiro.java
    IntelliJ nao passou o codigo pro Github.

--- src/Passageiro.java
+++ src/Passageiro.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Passageiro {
+    private String nome;
+    private String endereco;
+
+    public Passageiro(String nome, String endereco) {
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+    public void solicitarCorrida(String destino, Motorista motorista, Veiculo veiculo, double distancia, int tempo) {
+        motorista.iniciarCorrida(this, destino, veiculo, distancia, tempo);  //vai chamr motorista p/ iniciar corrida.
+    }
+
+    public void acompanharCorrida(Corrida corrida) {
+        System.out.println("Acompanhando a corrida...");
+        corrida.exibirDetalhes();
+    }
+    public void pagarCorrida(Corrida corrida) {
+        System.out.println("Corrida paga no valor de: R$ " + corrida.getPreco());
+    }
 }
