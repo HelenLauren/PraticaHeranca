2a283e8c9a497bfe587fdca0fd75dccb40a6b04f
Helen Lauren Bonato <167373993+HelenLauren@users.noreply.github.com>
2024-09-17 14:03:47 -0300

    Update Veiculo.java
    IntelliJ nao colocou o codigo no Github.

--- src/Veiculo.java
+++ src/Veiculo.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Veiculo {
+    private String modelo;
+    private String placa;
+    private String cor;
+
+    public Veiculo(String modelo, String placa, String cor){
+        this.modelo = modelo;
+        this.placa = placa;
+        this.cor = cor;
+    }
+    public void exibirDetalhes() {
+        System.out.println("Modelo: " + modelo + ", Placa: " + placa + ", Cor: " + cor);
+    }
 }
